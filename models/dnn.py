47addb240f83c9ba429465894f59da9cdb4210e3
shuyang1995 <41700715+shuyang1995@users.noreply.github.com>
2020-02-26 21:57:20 -0800

    Create dnn.py

---
+++ models/dnn.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler  
+
